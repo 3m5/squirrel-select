04c818a96ad480ea3a5cae7ddeb31fa236bd7950
schlosser@3m5.netz <eugen.schlosser@3m5.de>
2024-02-08 16:33:23 +0100

    add main and types in package.json

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,39 +1,35 @@
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
 import sass from 'sass'
 import scss from 'rollup-plugin-scss'
-import terser from '@rollup/plugin-terser';
+import terser from '@rollup/plugin-terser'
 import postcss from 'postcss'
 import autoprefixer from 'autoprefixer'
 
 export default {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: 'dist/index.js',
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: 'dist/index.min.js',
-        format: 'cjs',
-        plugins: [
-          terser()
-        ]
-      }],
-    plugins: [
-        peerDepsExternal(),
-        resolve(),
-        commonjs(),
-        typescript({ tsconfig: "./tsconfig.json" }),
-        scss({
-            fileName: 'css/squirrel-select.css',
-            processor: () => postcss([autoprefixer()]),
-            runtime: sass,
-            outputStyle: 'compressed',
-        })
-    ],
-};
+  input: 'src/index.ts',
+  output:
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+      plugins: [
+        terser()
+      ]
+    },
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json' }),
+    scss({
+      fileName: 'css/squirrel-select.css',
+      processor: () => postcss([autoprefixer()]),
+      runtime: sass,
+      outputStyle: 'compressed',
+    })
+  ],
+}
